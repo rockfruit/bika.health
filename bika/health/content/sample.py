5f89dbe76b455f8540c2c00f19764b08183c08e2
Pau Soliva <psoliva@naralabs.com>
2017-04-05 11:34:53 +0200

    Indexes and columns for sample catalog

--- bika/health/content/sample.py
+++ bika/health/content/sample.py
@@ -13,9 +13,14 @@ from bika.health.utiles.sample import getPatient
 
 # Defining the indexes for this extension. Since this is an extension, no
 # getter is created so we need to create indexes in that way.
-# TODO-catalog: delete this index
 @indexer(ISample, IBikaCatalogSampleListing)
 def getPatientUID(instance):
+    """
+    This index/column returns the patient UID for the sample.
+    :param instance: the sample to create the index.
+    :type instance: ATContentType
+    :returns: The patient UID as string
+    """
     patient = getPatient(instance)
     if patient:
         return patient.UID()
@@ -24,6 +29,12 @@ def getPatientUID(instance):
 
 @indexer(ISample, IBikaCatalogSampleListing)
 def getDoctorsUIDs(instance):
+    """
+    This index/column returns the doctors UIDs for the sample.
+    :param instance: the sample to create the index.
+    :type instance: ATContentType
+    :returns: The doctors UIDs as string
+    """
     ars = instance.getAnalysisRequests()
     doctors = []
     for ar in ars:
@@ -35,6 +46,12 @@ def getDoctorsUIDs(instance):
 
 @indexer(ISample, IBikaCatalogSampleListing)
 def getPatientID(instance):
+    """
+    This index/column returns the patient ID for the sample.
+    :param instance: the sample to create the index.
+    :type instance: ATContentType
+    :returns: The patient ID as string
+    """
     patient = getPatient(instance)
     if patient:
         return patient.getId()
@@ -43,6 +60,12 @@ def getPatientID(instance):
 
 @indexer(ISample, IBikaCatalogSampleListing)
 def getDoctorsIDs(instance):
+    """
+    This index/column returns the doctors IDs for the sample.
+    :param instance: the sample to create the index.
+    :type instance: ATContentType
+    :returns: The doctors IDs as string
+    """
     ars = instance.getAnalysisRequests()
     doctors = []
     for ar in ars:
@@ -53,10 +76,34 @@ def getDoctorsIDs(instance):
 
 
 @indexer(ISample, IBikaCatalogSampleListing)
-def getPatientTitle(instance):
+def getPatientTitleURL(instance):
+    """
+    This index/column returns the patient title and URL for the sample.
+    :param instance: the sample to create the index.
+    :type instance: ATContentType
+    :returns: a string with the patient's title and URL 'title|URL'
+    """
+    patient = getPatient(instance)
+    if patient:
+        return '|'.joint([patient.Title(), patient.absolute_url_path()])
     return ''
 
 
 @indexer(ISample, IBikaCatalogSampleListing)
-def getDoctorTitles(instance):
-    return ''
+def getDoctorTitlesURLs(instance):
+    """
+    This index/column returns the doctors titles and URLs for the sample.
+    :param instance: the sample to create the index.
+    :type instance: ATContentType
+    :returns: a string with doctors titles and URLs 'Doc1|URL1,Doc2|URL2,...'
+    """
+    ars = instance.getAnalysisRequests()
+    doctors_ids = []
+    titles_urls = []
+    for ar in ars:
+        doctor = ar.getField('Doctor').get(ar)
+        if doctor and doctor.getId() not in doctors_ids:
+            doctors.append(doctor.getId())
+            titles_urls.append(
+                '|'.join(doctor.Title(), doctor.absolute_url_path()))
+    return ','.join(titles_urls)
